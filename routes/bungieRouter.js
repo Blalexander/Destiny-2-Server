ef4394ca2fd9747815a68ccb51e015c1fc21ffbf
Blake Alexander <blake.alexander.13@gmail.com>
2019-03-14 18:25:09 -0700

    first and second route functional

--- routes/bungieRouter.js
+++ routes/bungieRouter.js
@@ -2,12 +2,9 @@ const axios = require("axios");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  // console.log(req.query.mtype, req.query.mname)
+router.get("/first", (req, res) => {
   console.log(req.query)
-  // console.log(req.params)
   var mname = req.query.mname.replace("#", "%23");
-
 
   let searchUrl = `https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/${req.query.mtype}/${mname}`;
   console.log(searchUrl);
@@ -25,13 +22,38 @@ router.get("/", (req, res) => {
       }
     )
     .then(payload => {
-      // console.log(payload.data)
       res.json(payload.data);
-      // getGuardianIds(payload.data);
     })
-    // .then(guardianIds => {
-    //   res.json(guardianIds);
-    // })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        message: "Something went wrong while querying Bungie"
+      });
+    });
+});
+
+router.get("/second", (req, res) => {
+  console.log(req.query)
+  // var mname = req.query.mname.replace("#", "%23");
+
+  // let searchUrl = `https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/${req.query.mtype}/${mname}`;
+  // console.log(searchUrl);
+
+  axios
+    .get(
+      `https://www.bungie.net/Platform/Destiny2/${req.query.mtype}/Profile/${req.query.mid}/?components=200`,
+      // searchUrl,
+      // `https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/4/Girthquake%2311226`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-Key": "62261ab05c7b4f078c05a94f18124761"
+        }
+      }
+    )
+    .then(payload => {
+      res.json(payload.data);
+    })
     .catch(err => {
       console.error(err);
       res.status(500).json({
@@ -41,8 +63,9 @@ router.get("/", (req, res) => {
 });
 
 // function getGuardianIds(userData) {
-//   let membershipType = userData.Response[0].membershipType;
-//   let membershipId = userData.Response[0].membershipId;
+//   // let membershipType = userData.Response[0].membershipType;
+//   // let membershipId = userData.Response[0].membershipId;
+//   // console.log(membershipType, membershipId);
 //   return(
 //     router.get("/", (req, res) => {
 //       axios
