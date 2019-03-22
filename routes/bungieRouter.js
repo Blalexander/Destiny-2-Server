a96994d3b63620e06b7c2f11b2ba17573d2c0bee
Blake Alexander <blake.alexander.13@gmail.com>
2019-03-22 10:32:50 -0700

    minor fixes to sequencing

--- routes/bungieRouter.js
+++ routes/bungieRouter.js
@@ -2,12 +2,117 @@ const axios = require("axios");
 const express = require("express");
 const router = express.Router();
 
+
+router.get("/second", (req, res) => {
+  keepTrackOfHowMany = 0;
+  allGames = [];
+  axios
+  .get(
+    `https://www.bungie.net/Platform/Destiny2/${req.query.mtype}/Account/${req.query.mid}/Character/${req.query.chid}/Stats/Activities/?mode=5&count=5`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-Key": "62261ab05c7b4f078c05a94f18124761"
+      }
+    }
+  )
+  .then(payload => {
+    // res.json(payload.data);
+    keepTrackOfHowMany = payload.data.Response.activities.length;
+    console.log(keepTrackOfHowMany);
+    // console.log(payload.data);
+
+    return payload.data;
+  })
+  .then(newPayload => {
+    newPayload.Response.activities.forEach(activity => {
+      allResponses.push(activity.activityDetails.referenceId);
+    })
+    // res.json(allResponses);
+    return allResponses;
+    //responds with an array of reference Ids
+  })
+  .then(activityArrayPayload => 
+    getAllDaStuff(activityArrayPayload)
+    // newItem.then(function(result) {
+    //   let newResult = result;
+    //   return(newResult);
+    // })
+    // console.log("newItem: ", newItem);
+    // if(newItem.resolve(1)) {
+    //   return(newItem);
+    // }
+  )
+  .then(payload => {
+    console.log("got here last");
+    res.json(payload);
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({
+      message: "Something went wrong while querying Bungie"
+    });
+  });
+})
+
+
+
+function getAllDaStuff(something) {
+  return new Promise(function(resolve, reject) {
+    something.forEach(refId => {
+      axios
+      .get(
+        `https://www.bungie.net/Platform/Destiny2/Stats/PostGameCarnageReport/${refId}/`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "X-API-Key": "62261ab05c7b4f078c05a94f18124761"
+          }
+        }
+      )
+      .then(payload => {
+        console.log(payload.data.Response.period);
+        // return payload.data;
+        // return payload.data;
+        // let newItem = payload.data;
+        allGames.push(payload.data);
+        console.log("KTOHM: ", keepTrackOfHowMany, "allGames: ", allGames.length);
+        // return allGames;
+        // if(keepTrackOfHowMany == allGames.length) {
+
+          return(allGames);
+        // }
+      })
+      .then(payload => {
+        if(keepTrackOfHowMany == payload.length) {
+          resolve(payload);
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({
+          message: "Something went wrong while querying Bungie"
+        });
+      });
+    })
+  })
+}
+
+
+let keepTrackOfHowMany = 0;
+
+let firstResponse;
 let secondResponse;
 let thirdResponse;
+let fourthResponse;
+let fifthResponse;
 let thirdData;
 let firstSecondObj;
 let fstObj;
-
+let secondSet;
+let thirdSet;
+let allResponses = [];
+let allGames = [];
 let characterIdArray;
 let characterId1;
 let membershipType;
@@ -33,95 +138,45 @@ router.get("/first", (req, res) => {
         }
       }
     )
-    .then(payload1 => {
-      return secondApiCall(payload1.data);
-    })
-    .then(payload2 => {
-      return thirdApiCall(payload2.data);
-    })
     .then(payload => {
-      // console.log("Goal reached");
-      // thirdData = payload;
-      fstObj = Object.assign({}, secondResponse, thirdResponse, thirdData);
-      console.log("Goal reached, fstObj", fstObj);
-      res.json(fstObj);
+      firstResponse = payload.data.Response;
+      console.log("firstResponse: ", firstResponse);
+      membershipType = firstResponse[0].membershipType;
+      membershipId = firstResponse[0].membershipId;
+      console.log(membershipType, membershipId);
+    
+      //RES .JSON SOMEWHERE
+
+      axios
+      .get(
+        `https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=200`,
+        // searchUrl,
+        // `https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/4/Girthquake%2311226`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "X-API-Key": "62261ab05c7b4f078c05a94f18124761"
+          }
+        }
+      )
+      .then(payload2 => {
+        secondResponse = payload2.data.Response;
+        let nameAndCharacters = Object.assign({}, firstResponse, secondResponse);
+        res.json(nameAndCharacters);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({
+          message: "Something went wrong while querying Bungie"
+        });
+      });
     })
     .catch(err => {
-      console.error(err);
-      res.status(500).json({
-        message: "Something went wrong while querying Bungie"
-      });
+        console.error(err);
+        res.status(500).json({
+          message: "Something went wrong while querying Bungie"
+        });
     });
 });
-
-function secondApiCall(secondPayload) {
-  // console.log("secondPayload: ", secondPayload);
-  secondResponse = secondPayload.Response;
-  console.log("secondResponse: ", secondResponse);
-  // res.json(payload.data);
-  membershipType = secondResponse[0].membershipType;
-  membershipId = secondResponse[0].membershipId;
-  console.log(membershipType, membershipId);
-
-  axios
-  .get(
-    `https://www.bungie.net/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=200`,
-    // searchUrl,
-    // `https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/4/Girthquake%2311226`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "X-API-Key": "62261ab05c7b4f078c05a94f18124761"
-      }
-    }
-  )
-  .then(secondRequest => {
-    console.log("Returning second request");
-    // return secondRequest;
-    return console.log(secondRequest.data);
-  })
-  .catch(err => {
-    console.error(err);
-    res.status(500).json({
-      message: "Something went wrong while querying Bungie"
-    });
-  });
-}
-
-
-function thirdApiCall(thirdPayload) {
-  // console.log("thirdPayload: ", thirdPayload);
-  thirdResponse = thirdPayload.Response;
-  console.log("thirdResponse: ", thirdResponse);
-  firstSecondObj = Object.assign({}, secondResponse, thirdResponse);
-  // console.log("Third data: ", thirdData);
-  console.log("firstSecondObj: ", firstSecondObj);
-  // console.log("keys: ", Object.keys(firstSecondObj.characters.data));
-
-  axios
-  .get(
-    `https://www.bungie.net/Platform/Destiny2/${membershipType}/Account/${membershipId}/Character/${Object.keys(firstSecondObj.characters.data)[0]}/Stats/Activities/?mode=5&count=5`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "X-API-Key": "62261ab05c7b4f078c05a94f18124761"
-      }
-    }
-  )
-  .then(thirdRequest => {
-    console.log("Returning third request");
-    return thirdRequest;
-    // thirdData = payload.data.Response;
-    // fstObj = Object.assign({}, secondResponse, thirdResponse, thirdData);
-    // // return console.log(fstObj);
-    // return (fstObj);
-  })
-  .catch(err => {
-    console.error(err);
-    res.status(500).json({
-      message: "Something went wrong while querying Bungie"
-    });
-  });
-}
 
 module.exports = router;
