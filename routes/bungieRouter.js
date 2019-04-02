ec67922ee137d1fb2fde683957ccccd5fabebabc
Blake Alexander <blake.alexander.13@gmail.com>
2019-04-02 09:42:31 -0700

    cleaned up code for readability

--- routes/bungieRouter.js
+++ routes/bungieRouter.js
@@ -22,10 +22,7 @@ let allGames = [];
 
 const pg = PGCR();
 
-// getSecond(4, 4611686018470723268, 2305843009301006557);
-// function getSecond(mtype, mid, chid) {
 router.get("/second", (req, res) => {
-  // const pg = PGCR();
   keepTrackOfHowMany = 0;
   allResponses = [];
   let saveThis;
@@ -56,21 +53,6 @@ router.get("/second", (req, res) => {
   )
   .then(payload => {
     console.log("got here last");
-
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    // let insertionObj = {gamesPlayed: saveThis, masterArr: payload};
-
-    // pg.collection.insert(insertionObj, onInsert);
-  
-    // function onInsert(err, docs) {
-    //   if (err) {
-    //     console.log("Error!", err);
-    //   } else {
-    //     console.info("PGCRs were successfully stored.", docs.length);
-    //   }
-    // }
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
     res.json(payload);
   })
   // .catch(err => {
@@ -79,95 +61,8 @@ router.get("/second", (req, res) => {
   //     message: "Something went wrong while querying Bungie"
   //   });
   // });
-})/*?+*/
+})
 
-
-// let theArrrr = [ 1153409123, 1815340083, 399506119, 806094750, 2748633318 ];
-// getAllDaStuff(theArrrr);
-// function getAllDaStuff(something) {
-//   console.log(something);
-//   allGames = [];
-//   return new Promise(function(resolve, reject) {
-//     something.forEach(refId => {
-//       axios
-//       .get(
-//         `https://www.bungie.net/Platform/Destiny2/Stats/PostGameCarnageReport/${refId}/`,
-//         {
-//           headers: {
-//             "Content-Type": "application/json",
-//             "X-API-Key": "62261ab05c7b4f078c05a94f18124761"
-//           }
-//         }
-//       )
-//       .then(payload => {
-//         console.log(payload.data.Response);
- 
-//         let refIdForPGCR = payload.data.Response.activityDetails.instanceId;
-//         let entriesForPGCR = payload.data.Response.entries
-//         let bigArray = [];
-
-//         entriesForPGCR.forEach(async function(entry) {
-//           let nameForPGCR = entry.characterId;
-//           bigArray.push(nameForPGCR);
-//           console.log("STARTING");
-
-//           const xpvar = await fff();
-          
-//           function fff() {
-//             return new Promise(resolve => {
-//               PGCR.findOne({characterId: nameForPGCR}).then(load => {
-//                 if(load === null) { //inserts if not there
-//                   console.log(nameForPGCR, " has no record.  Inserting record now.");
-//                   // let insertionObj = {characterId: nameForPGCR, referenceIdsForGamesPlayed: [refIdForPGCR], gameEntries: entry};
-//                   let insertionObj = {characterId: nameForPGCR, referenceIdsForGamesPlayed: [refIdForPGCR]};
-//                   resolve(pg.collection.insert(insertionObj, onInsert));
-                
-//                   function onInsert(err) {
-//                     if (err) {
-//                       console.log("Error!", err);
-//                     } else {
-//                       console.info("PGCRs were successfully stored.");
-//                     }
-//                   }
-//                 }
-//                 else if(load.referenceIdsForGamesPlayed.includes(refIdForPGCR)) { 
-//                   console.log("Record for " + refIdForPGCR + " in " + nameForPGCR + " found!");
-//                   resolve(pg.collection.update({characterId: nameForPGCR}, {$push: {referenceIdsForGamesPlayed: refIdForPGCR}}));
-//                   // console.log("Game already in character's history!");
-//                 }
-//                 else { //updates if account present but no ref id yet
-//                   console.log("Record found for account, updating history now!");
-//                   resolve(pg.collection.update({characterId: nameForPGCR}, {$push: {referenceIdsForGamesPlayed: refIdForPGCR}}));
-//                 }
-//               })
-//             })
-//           }
-        
-//           return xpvar;
-//         })
-
-//         return(allGames);
-//       })
-//       .then(payload => {
-//         if(keepTrackOfHowMany == payload.length) {
-//           console.log("resolving payload");
-//           resolve(payload);
-//         }
-//       })
-//       .catch(err => {
-//         console.error(err);
-//         err.status(500).json({
-//           message: "Something went wrong while querying Bungie"
-//         });
-//       });
-//     })
-//   })
-// }
-
-//YO THE CHARACTERID YOU'RE USING IS PROBABLY FOR THE OVERALL ACCOUNT/THE ID IS WRONG
-
-// getFromDB();
-// function getFromDB() {
 router.get('/hope', jsonParser, (req, res) => {
   // console.log(req.body);
   // const myCursor = PGCR.find({}, {gamesPlayed: { $slice: 2 }}); 
@@ -302,40 +197,6 @@ Array.prototype.unique = function() {
 
 
 
-// function getAllDaStuff(something) {
-//   allGames = [];
-//   return new Promise(function(resolve, reject) {
-//     something.forEach(refId => {
-//       axios
-//       .get(
-//         `https://www.bungie.net/Platform/Destiny2/Stats/PostGameCarnageReport/${refId}/`,
-//         {
-//           headers: {
-//             "Content-Type": "application/json",
-//             "X-API-Key": "62261ab05c7b4f078c05a94f18124761"
-//           }
-//         }
-//       )
-//       .then(payload => {
-//         console.log(payload.data.Response.period);
-//         allGames.push(payload.data);
-//         console.log("KTOHM: ", keepTrackOfHowMany, "allGames: ", allGames.length);
-//         return(allGames);
-//       })
-//       .then(payload => {
-//         if(keepTrackOfHowMany == payload.length) {
-//           console.log("resolving payload");
-//           resolve(payload);
-//         }
-//       })
-//       .catch(err => {
-//         allGames.push("404");
-//         console.log("Throwing a 404!");
-//         return(allGames);
-//       });
-//     })
-//   })
-// }
 
 
 let firstResponse;
@@ -404,45 +265,4 @@ router.get("/first", (req, res) => {
     });
 });
 
-// router.use('*', function (req, res) {
-//   res.status(404).json({ message: 'Not Found' });
-// });
-
 module.exports = router;
-
-
-
-// if(load === null) { //inserts if not there
-//   console.log(nameForPGCR, " has no record.  Inserting record now.");
-//   // let insertionObj = {characterId: nameForPGCR, referenceIdsForGamesPlayed: [refIdForPGCR], gameEntries: entry};
-//   let insertionObj = {characterId: nameForPGCR, referenceIdsForGamesPlayed: [refIdForPGCR]};
-//   pg.collection.insert(insertionObj, onInsert);
-
-//   function onInsert(err) {
-//     if (err) {
-//       console.log("Error!", err);
-//     } else {
-//       console.info("PGCRs were successfully stored.");
-//     }
-//   }
-// }
-// else if(load.referenceIdsForGamesPlayed.includes(refIdForPGCR)) { 
-//   console.log("Record for " + refIdForPGCR + " in " + nameForPGCR + " found!");
-//   pg.collection.update({characterId: nameForPGCR}, {$push: {referenceIdsForGamesPlayed: refIdForPGCR}});
-//   // console.log("Game already in character's history!");
-// }
-// else { //updates if account present but no ref id yet
-//   console.log("Record found for account, updating history now!");
-//   pg.collection.update({characterId: nameForPGCR}, {$push: {referenceIdsForGamesPlayed: refIdForPGCR}});
-// }
-// return bigArray;
-// })///////////////////////////////////////THAT ALL WORKS ^, NOW, CONDENSE RESULTS AFTER EACH TIMES IT'S RUN //OR MAKE EACH SAVE A PROMISE AND AVOID ANY CHECKING AFTERWARD (BETTEREST?)
-// .then(biggie => {
-// biggie.forEach(charId => {
-//   PGCR.find({characterId: charId}).then(loads => {
-//     if(loads.length > 1) {
-//       pg.collection.update({characterId: charId}, {$push: {referenceIdsForGamesPlayed: refIdForPGCR}});
-//     }
-//   })
-// })
-// })
