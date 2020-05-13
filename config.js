5f91dd50012ed20b300732a860820b9950443c7b
Blake Alexander <blake.alexander.13@gmail.com>
2020-05-13 13:30:47 -0700

    increased heap memory

--- config.js
+++ config.js
@@ -22,3 +22,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://blake:blake1@ds131
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.NODE_OPTIONS="--max_old_space_size=4096"
