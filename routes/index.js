cf40f983e514c00aeddd99d400be20c1c1918a07
Blake Alexander <blake.alexander.13@gmail.com>
2021-06-25 14:57:17 -0700

    initial config

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const { PGCR } = require('./models');
-const { Mani } = require('./models');
-const { router } = require('./bungieRouter');
-
-module.exports = { PGCR, Mani, router };
